9e8bee3c3f5b702f161106a730b3ed4106fc332a
comphonia <online.comphonia@gmail.com>
2018-11-05 21:51:40 -0600

    Revert "Bumper Project"
    This reverts commit fc02dce6774c569141225176e963d7a86e3ebdb2.

---
+++ Assets/FPS Characters/Demo/demo.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace T23DS
+{
+    //Simple Script for Demo scene and is not required for your project
+
+    public class demo : MonoBehaviour
+    {
+
+        public int counter = 0;
+        public Text CharacterName;
+        public GameObject[] character;
+        private textTo3dSpeech t23ds;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (character.Length > 0)
+            {
+                CharacterName.text = character[counter].transform.name;
+            }
+        }
+
+        void Update()
+        {
+            if (character.Length > 0)
+            {
+                t23ds = character[counter].GetComponent<textTo3dSpeech>();
+            }
+        }
+
+        public void next()  // Called by Next Button
+        {
+
+            if (counter < character.Length - 1)
+            {
+                counter++;
+                character[counter].SetActive(true);
+                character[counter - 1].SetActive(false);
+                CharacterName.text = character[counter].transform.name;
+
+            }
+            else
+            {
+                character[counter].SetActive(false);
+                counter = 0;
+                character[counter].SetActive(true);
+                CharacterName.text = character[counter].transform.name;
+            }
+        }
+
+        public void prev() // Called by Previous Button
+        {
+            if (counter > 0)
+            {
+                counter--;
+                character[counter].SetActive(true);
+                character[counter + 1].SetActive(false);
+                CharacterName.text = character[counter].transform.name;
+
+            }
+            else
+            {
+                character[counter].SetActive(false);
+                counter = character.Length - 1;
+                character[counter].SetActive(true);
+                CharacterName.text = character[counter].transform.name;
+            }
+        }
+
+    }
+}
---
+++ Assets/FPS Characters/Demo/sceneassets/demo.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+    //Simple Script for Demo scene and is not required for your project
+
+    public class demo : MonoBehaviour
+    {
+
+        public int counter = 0;
+        public Text speakerName;
+        public GameObject[] character;
+        bool RLeft = false, RRight = false;
+
+    // Use this for initialization
+    void Start()
+        {
+            if (character.Length > 0)
+            {
+                speakerName.text = character[counter].transform.name;
+            }
+        }
+
+
+        public void next()  // Called by Next Button
+        {
+
+            if (counter < character.Length - 1)
+            {
+                counter++;
+                character[counter].SetActive(true);
+                character[counter - 1].SetActive(false);
+                speakerName.text = character[counter].transform.name;
+
+            }
+            else
+            {
+                character[counter].SetActive(false);
+                counter = 0;
+                character[counter].SetActive(true);
+                speakerName.text = character[counter].transform.name;
+            }
+        }
+
+        public void prev() // Called by Previous Button
+        {
+            if (counter > 0)
+            {
+                counter--;
+                character[counter].SetActive(true);
+                character[counter + 1].SetActive(false);
+                speakerName.text = character[counter].transform.name;
+
+            }
+            else
+            {
+                character[counter].SetActive(false);
+                counter = character.Length - 1;
+                character[counter].SetActive(true);
+                speakerName.text = character[counter].transform.name;
+            }
+        }
+
+    public void RotateRight()
+    {
+        RRight = true;
+    }
+    public void RotateRightFalse()
+    {
+        RRight = false;
+    }
+
+    public void RotateLeft()
+    {
+        RLeft = true;
+    }
+    public void RotateLeftFalse()
+    {
+        RLeft = false;
+    }
+
+    void Update()
+    {
+        if(RLeft)
+        {
+            character[counter].transform.Rotate(Vector3.up * 3);
+        }
+        if (RRight)
+        {
+            character[counter].transform.Rotate(Vector3.down * 3);
+        }
+    }
+
+}
